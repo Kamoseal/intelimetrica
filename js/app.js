512e4992e6fdf0b055e203c05d7fb218314256e0
Kamoseal <serna.karla@gmail.com>
2019-04-24 00:11:22 -0500

    Pintando informacion en HTML

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+//extracting div from html
+const restaurants = document.getElementById('restaurants');
+
 //Name URL
 const requesURL = 'https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json';
 //creating object
@@ -11,5 +14,53 @@ request.send();
 //waiting for the response to restart from the server to handle it
 request.onload = function() {
     const dataRestaurant = request.response;
-    console.log(dataRestaurant);
+    //painting data in console
+    //console.log(dataRestaurant);
+
+    //creating template
+    let template = ``
+    //tour array to extract information and assign name
+    dataRestaurant.forEach(arrayRestaurants => {
+    //console.log(arrayRestaurants);
+        const idRestaurant = arrayRestaurants.id;
+        const rating = arrayRestaurants.rating;
+        const name = arrayRestaurants.name;
+        const site = arrayRestaurants.contact.site;
+        const email = arrayRestaurants.contact.email;
+        const phone = arrayRestaurants.contact.phone;
+        const street = arrayRestaurants.address.street;
+        const city = arrayRestaurants.address.city;
+        const state = arrayRestaurants.address.state;
+        const lat = arrayRestaurants.address.location.lat;
+        const ing = arrayRestaurants.address.location.ing;
+
+        //assign HTML code to the template with values ​​extracted from the data
+        template +=
+        `<ul>
+            <li>${name}</li>
+            <li>${rating}</li>
+            <li>${street, city, state}</li>
+            <li>${phone}</li>
+            <li>${site}</li>
+            <li>${email}</li>
+        </ul>`;
+            //painting information in HTML
+            restaurants.innerHTML = template;
+    
+    });
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
